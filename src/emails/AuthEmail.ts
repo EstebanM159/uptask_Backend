f75e3693865dab06ce638ec1a4a6285874a02918
EstebanM159 <estebanm91231@gmail.com>
2024-05-23 15:27:50 -0300

    Add sistemMail

--- src/emails/AuthEmail.ts
+++ src/emails/AuthEmail.ts
@@ -1,5 +1,8 @@
-import { transporter } from "../config/nodemailers"
 // agregar userId
+import { Resend } from 'resend';
+import dotenv from 'dotenv'
+dotenv.config()
+const resend = new Resend(`${process.env.RESEND_API_KEY}`);
 interface IEmail {
     email:string,
     name:string,
@@ -8,36 +11,40 @@ interface IEmail {
 }
 export class AuthEmail {
     static sendConfirmationEmail = async (user:IEmail)=>{
-        const info = await transporter.sendMail({
-                from: 'UpTask <admin@uptask.com>',
-                to : user.email,
-                subject: 'Uptask - Confirma tu cuenta',
-                text: 'Uptask - Confirma tu cuenta',
-                html: `<p>Hola ${user.name}, has creado tu cuenta en UpTask, ya casi esta todo listo solo debes confirmar tu cuenta</p>
-                    <p>Visita el siente enlace: </p> <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
-                    <p>E ingresa el siguiente codigo: <b>${user.token}</b></p>
-                    <p>Este token expira en 10 m</p>
-                
-                
-                `
-            })
-        console.log('Mensaje enviado', info.messageId)
-    }
+        
+        const { data, error } = await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: [`${user.email}`],
+            subject: 'Uptask - Confirma tu cuenta',
+            html: `<p>Hola ${user.name}, has creado tu cuenta en UpTask, ya casi esta todo listo solo debes confirmar tu cuenta</p>
+                            <p>Visita el siente enlace: </p> <a href="${process.env.FRONTEND_URL}/auth/confirm-account">Confirmar cuenta</a>
+                            <p>E ingresa el siguiente codigo: <b>${user.token}</b></p>
+                            <p>Este token expira en 10 m</p>`,
+        });
+
+        if (error) {
+            return console.error({ error });
+        }
+
+        console.log({ data });
+}
+
     static sendPasswordResetToken = async (user:IEmail)=>{
-        const info = await transporter.sendMail({
-                from: 'UpTask <admin@uptask.com>',
-                to : user.email,
+        const { data, error } = await resend.emails.send({
+                from: 'Acme <onboarding@resend.dev>',
+                to : [`${user.email}`],
                 subject: 'Uptask - Reestablece tu password',
-                text: 'Uptask - Reestablece tu password',
                 html: `<p>Hola ${user.name}, has solicitado reestablecer tu password</p>
                     <p>Visita el siente enlace: </p> <a href="${process.env.FRONTEND_URL}/auth/new-password/${user.tokenId}">Reestablecer password</a>
                     <p>E ingresa el siguiente codigo: <b>${user.token}</b></p>
                     <p>Este token expira en 10 m</p>
-                
-                
                 `
             })
-        console.log('Mensaje enviado', info.messageId)
+        if (error) {
+            return console.error({ error });
+        }
+
+        console.log({ data });
     }
     
 }
